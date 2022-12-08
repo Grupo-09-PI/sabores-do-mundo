2ce4a2c73e0b4bfc03d00df4114db45be3ac248d
Guilherme Januário <guiijanuario@gmail.com>
2022-12-08 00:35:45 -0300

    Iniciando add produto no localstorage

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -4,6 +4,6 @@ module.exports = {
     showIndex: (req, res) => {
         const produtosIndex = ProdutosIndexModels.index();
         //controler comunicando com a view
-        return res.render('index', {produtosIndex, tittle: 'Sabores do mundo', css: [ 'modalLogin.css', 'style.css', 'slider.css'], javascript: ['slider.js', 'modalLogin.js', 'hamburger.js' ] })
+        return res.render('index', {produtosIndex, tittle: 'Sabores do mundo', css: [ 'modalLogin.css', 'style.css', 'slider.css'], javascript: ['slider.js', 'modalLogin.js', 'hamburger.js', 'carrinho.js' ] })
     }
 }
