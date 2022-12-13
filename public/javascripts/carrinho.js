36da16029c8bb9409d773eada38d19ee9662a219
guilherme <guiijanuario@gmail.com>
2022-12-13 01:01:25 -0300

    corrigindo o layout do carrinho

--- public/javascripts/carrinho.js
+++ public/javascripts/carrinho.js
@@ -4,6 +4,7 @@ const conteinerCarrinho = document.querySelector('.tabela-itens-carrinho');
 const carrinho = JSON.parse(localStorage.getItem('carrinho'));
 
 carrinho.forEach(item => {
+
     conteinerCarrinho.innerHTML += `
     <div class="tabela-itens-carrinho">
                 <div class="linha-de-produto" id="first-produto">
@@ -14,16 +15,16 @@ carrinho.forEach(item => {
                         <h3>${item.nome}</h3>
                     </div>
                     <div class="preco-unitário" >
-                        <h4>${item.valorVenda}</h4>
+                        <h4>${item.valor}</h4>
                     </div>
                     <div class="quantidade-produto">
-                        <input type="number" placeholder="Qtd">
+                        <input type="number" placeholder="Qtd" value="${item.quantidade}" > 
                     </div>
                     <div class="subtotal">
-                        <h4></h4>
+                        <h4>${item.valor}</h4>
                     </div>
                     <div class="excluir">
-                        <a href="#"><img src="../imagens/body-main/lixeira.png" alt=""></a>
+                        <a href="#"><img src="../images/body-main/lixeira.png" alt=""></a>
                     </div>
                 </div>
             </div>
