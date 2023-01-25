3fa20ae6e7468b51f6364b10c465ab94066367c8
Guilherme Januário <guiijanuario@gmail.com>
2023-01-24 22:38:10 -0300

    Criando rota da pagina de produto pelo parametro de ID com get

--- src/controller/produtoController.js
+++ src/controller/produtoController.js
@@ -3,5 +3,8 @@ module.exports = {
     showIndex: (req, res) => {
         //controler comunicando com a view
         return res.render('produto', {tittle: 'Produto', css: [ 'modalLogin.css', 'style.css', 'produto.css'], javascript: ['modalLogin.js']})
+    },
+    produtoId: (req, res) => {
+        return res.render('produto', {tittle: 'Produto', css: [ 'modalLogin.css', 'style.css', 'produto.css'], javascript: ['modalLogin.js']})
     }
 }
